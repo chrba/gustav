cbfcafd3c4e8b99ffa9acac215d12d04c6178930
Christian Bannes <cbannes@gmail.com>
2015-09-11 23:25:32 +0200

    use resize policy for table

---
+++ src/main/java/de/chrb/gustav/model/statistics/Characteristics.java
@@ -0,0 +1,11 @@
+package de.chrb.gustav.model.statistics;
+
+public class Characteristics {
+
+	//allocation rate 
+	//promotion rate
+	//surviver death ratio
+	//old gen colletion time
+	//young gen collection time
+	//old gen buf
+}
