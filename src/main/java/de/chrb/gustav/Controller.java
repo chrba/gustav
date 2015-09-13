0e7093852b21d7d9b2010c75475e18ac820cd5bc
Christian Bannes <cbannes@gmail.com>
2015-09-13 21:32:48 +0200

    replace textarea with htmleditor

--- src/main/java/de/chrb/gustav/Controller.java
+++ src/main/java/de/chrb/gustav/Controller.java
@@ -2,7 +2,9 @@ package de.chrb.gustav;
 
 import java.io.File;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,10 +14,12 @@ import de.chrb.gustav.model.parser.ParserRegistry;
 import de.chrb.gustav.model.statistics.Characteristics;
 import de.chrb.gustav.model.statistics.Statistics;
 import de.chrb.gustav.model.statistics.StatisticsAnalyzer;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.LineChart;
@@ -27,6 +31,9 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.HTMLEditor;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -110,7 +117,7 @@ public class Controller {
     private Button btnAdd;
 
     @FXML
-    private TextArea tfImportantDataTextArea;
+    private HTMLEditor tfImportantDataTextArea;
 
     @FXML
     private BarChart<String, Double> bcSigma;
@@ -118,6 +125,23 @@ public class Controller {
 	private ParserRegistry parserRegistry;
 
 
+	@FXML
+	public void initialize() {
+		hideHTMLEditorToolbars(tfImportantDataTextArea);
+	}
+
+	public static void hideHTMLEditorToolbars(final HTMLEditor editor)
+	{
+	    editor.setVisible(false);
+	    Platform.runLater(() -> {
+		    Node[] nodes = editor.lookupAll(".tool-bar").toArray(new Node[0]);
+		    Arrays.stream(nodes).forEach(n -> {
+	            n.setVisible(false);
+	            n.setManaged(false);
+		    });
+	        editor.setVisible(true);
+	    });
+	}
 
     @FXML
     void addNewFile(ActionEvent event) {
@@ -180,7 +204,7 @@ public class Controller {
     	pcGCStats.setData(FXCollections.observableArrayList(analyzer.createPieChartData()));
 
     	final Characteristics c = new Characteristics(events);
-    	tfImportantDataTextArea.setText(c.text());
+    	tfImportantDataTextArea.setHtmlText(c.text());
 
     }
 
