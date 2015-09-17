07fb21dd7a07d8b6acad60a8034b0c1d32e8e58e
Christian Bannes <cbannes@gmail.com>
2015-09-17 22:38:20 +0200

    move bar charts into its own fxml file

---
+++ src/main/java/de/chrb/gustav/controller/BarChartsController.java
@@ -0,0 +1,36 @@
+package de.chrb.gustav.controller;
+
+import de.chrb.gustav.model.statistics.StatisticsAnalyzer;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+
+/**
+ * Controller containing the bar chart statistics
+ *
+ * @author Christian Bannes
+ */
+public class BarChartsController {
+
+    @FXML private BarChart<String, Double> avg;
+    @FXML private BarChart<String, Long> min;
+    @FXML private BarChart<String, Double> totalGC;
+    @FXML private BarChart<String, Double> overhead;
+    @FXML private BarChart<String, Integer> num;
+    @FXML private BarChart<String, Long> max;
+    @FXML private BarChart<String, Double> numPerc;
+    @FXML private BarChart<String, Double> sigma;
+
+
+	public void addSeries(StatisticsAnalyzer analyzer) {
+    	this.num.getData().add(analyzer.createNumSeries());
+    	this.overhead.getData().add(analyzer.createOverheadSeries());
+    	this.avg.getData().add(analyzer.createAvgSeries());
+    	this.max.getData().add(analyzer.createMaxSeries());
+    	this.min.getData().add(analyzer.createMinSeries());
+    	this.sigma.getData().add(analyzer.createSigmaSeries());
+    	this.numPerc.getData().add(analyzer.createNumPercSeries());
+    	this.totalGC.getData().add(analyzer.createSecsPercSeries());
+
+	}
+
+}
--- src/main/java/de/chrb/gustav/controller/MainController.java
+++ src/main/java/de/chrb/gustav/controller/MainController.java
@@ -1,10 +1,8 @@
 package de.chrb.gustav.controller;
 
 import java.io.File;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import de.chrb.gustav.model.gc.GCEvent;
 import de.chrb.gustav.model.parser.ParserRegistry;
 import de.chrb.gustav.model.statistics.Characteristics;
-import de.chrb.gustav.model.statistics.Statistics;
 import de.chrb.gustav.model.statistics.StatisticsAnalyzer;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
@@ -21,18 +18,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -40,66 +30,16 @@ import javafx.stage.Stage;
 public class MainController {
 	private static Logger LOG = LoggerFactory.getLogger(MainController.class);
 
-	@FXML
-	private Parent root;
-
-    @FXML
-    private BarChart<String, Double> bcAvg;
-
-
-
-    @FXML
-    private ScatterChart<Double, Double> lcGCTimeline;
-
-
-    @FXML
-    private BarChart<String, Long> bcMin;
-
-    @FXML
-    private ScatterChart<Long, Long> lcGCPauseDistribution;
-
-    @FXML
-    private BarChart<String, Double> bcTotalGC;
-
-
-    @FXML
-    private BarChart<String, Double> bcOverhead;
-
-    @FXML
-    private BarChart<String, Integer> bcNum;
-
-
-
-    @FXML
-    private Button btnRemove;
-
-
-    @FXML
-    private ListView<String> fileListView;
-
-
-    @FXML
-    private PieChart pcGCStats;
-
-    @FXML
-    private BarChart<String, Long> bcMax;
-
-    @FXML
-    private BarChart<String, Double> bcNumPerc;
-
-
-
-    @FXML
-    private Button btnAdd;
-
-    @FXML
-    private HTMLEditor tfImportantDataTextArea;
-
-    @FXML
-    private BarChart<String, Double> bcSigma;
-
-    @FXML
-    private StatTableController statTableViewController;
+	@FXML private Parent root;
+    @FXML private ScatterChart<Double, Double> lcGCTimeline;
+    @FXML private ScatterChart<Long, Long> lcGCPauseDistribution;
+    @FXML private Button btnRemove;
+    @FXML private ListView<String> fileListView;
+    @FXML private PieChart pcGCStats;
+    @FXML private Button btnAdd;
+    @FXML private HTMLEditor tfImportantDataTextArea;
+    @FXML private StatTableController statTableViewController;
+    @FXML BarChartsController barChartsViewController;
 
 	private ParserRegistry parserRegistry;
 
@@ -139,21 +79,9 @@ public class MainController {
     	final StatisticsAnalyzer analyzer = new StatisticsAnalyzer(events);
     	this.statTableViewController.addData(analyzer.statisticsList());
 
-
-    	//final ObservableList<String> items = FXCollections.observableArrayList("file:" + file.getName());
     	files.add("file: " + file.getName());
 
-
-
-    	bcNum.getData().add(analyzer.createNumSeries());
-    	bcOverhead.getData().add(analyzer.createOverheadSeries());
-    	bcAvg.getData().add(analyzer.createAvgSeries());
-    	bcMax.getData().add(analyzer.createMaxSeries());
-    	bcMin.getData().add(analyzer.createMinSeries());
-    	bcSigma.getData().add(analyzer.createSigmaSeries());
-    	bcNumPerc.getData().add(analyzer.createNumPercSeries());
-    	bcTotalGC.getData().add(analyzer.createSecsPercSeries());
-
+    	barChartsViewController.addSeries(analyzer);
 
     	//
     	//lcGCTimeline
